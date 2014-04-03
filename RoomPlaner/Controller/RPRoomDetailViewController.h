9fcc1b57ddca5998b59700ce7a2836676e413f75
Paul Ehrhardt <paul.ehrhardt@gmx.net>
2014-04-03 20:58:23 +0200

    added room detail view controller

---
+++ RoomPlaner/Controller/RPRoomDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  RPRoomDetailViewController.h
+//  RoomPlaner
+//
+//  Created by Paul Ehrhardt on 03.04.14.
+//  Copyright (c) 2014 Hackathon. All rights reserved.
+//
+
+@class Room;
+
+@interface RPRoomDetailViewController : UIViewController
+@property (strong, nonatomic) Room *room;
+@end
