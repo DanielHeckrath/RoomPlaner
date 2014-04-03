d164d0b2b2a871f485e2b65915b59978657cccf8
Ömer Avci <oemer.a@me.com>
2014-04-03 20:00:29 +0200

    added room base cell

--- RoomPlaner/Controller/RPRoomListTableViewController.m
+++ RoomPlaner/Controller/RPRoomListTableViewController.m
@@ -7,117 +7,65 @@
 //
 
 #import "RPRoomListTableViewController.h"
+#import "Room.h"
+#import "RPRoomAvailabilityTableViewCell.h"
+
+static NSString * const CELL_IDENTIFIER = @"RPRoomTableViewCell";
 
 @interface RPRoomListTableViewController ()
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *addButton;
-
+@property (nonatomic, strong) NSArray *rooms;
 @end
 
 @implementation RPRoomListTableViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
+    self.tableView.allowsSelection = NO;
+    [self.tableView registerClass:[RPRoomAvailabilityTableViewCell class] forCellReuseIdentifier:CELL_IDENTIFIER];
+
+    self.rooms = [self mockedRooms];
+}
+
+- (NSArray *)mockedRooms {
+    Room *firstRoom = [[Room alloc] init];
+    firstRoom.name = @"Startplatz 01";
+    firstRoom.occupied = YES;
+    Room *secondRoom = [[Room alloc] init];
+    secondRoom.name = @"Startplatz 02";
+    secondRoom.occupied = YES;
+    Room *thirdRoom = [[Room alloc] init];
+    thirdRoom.name = @"Startplatz 03";
+    thirdRoom.occupied = YES;
+    return @[firstRoom, secondRoom, thirdRoom];
 }
 
 #pragma mark -
 #pragma mark - Table view data source
 
-- (IBAction)addAction:(UIBarButtonItem *)sender
-{
-    
+- (IBAction)addAction:(UIBarButtonItem *)sender {
+    // TODO: show add view
+}
+
+#pragma mark -
+#pragma mark - UITableViewDelegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 60;
 }
 
 #pragma mark -
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.rooms count];
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    RPRoomAvailabilityTableViewCell *roomTableViewCell = [tableView dequeueReusableCellWithIdentifier:CELL_IDENTIFIER];
+    roomTableViewCell.room = self.rooms[(NSUInteger) indexPath.row];
+    return roomTableViewCell;
 }
-
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
-}
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
---
+++ RoomPlaner/View/RPRoomAvailabilityTableViewCell.h
@@ -0,0 +1,13 @@
+//
+// RPRoomAvailabilityTableViewCell
+// RoomPlaner 
+// 
+// Created by Ömer Avci on 03.04.14.
+// Copyright (c) 2014 Hackathon. All rights reserved.
+//
+
+@class Room;
+
+@interface RPRoomAvailabilityTableViewCell : UITableViewCell
+@property (nonatomic, strong) Room *room;
+@end
