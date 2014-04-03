7eb1f9a8a56d71489dfca1f89c68e77a0caa8e0a
Daniel Heckrath <d.heckrath@maple-apps.com>
2014-04-03 19:01:35 +0200

    Project setup

---
+++ RoomPlaner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RoomPlaner
+//
+//  Created by Daniel Heckrath on 03.04.14.
+//  Copyright (c) 2014 Hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RoomPlaner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RoomPlaner
+//
+//  Created by Daniel Heckrath on 03.04.14.
+//  Copyright (c) 2014 Hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
